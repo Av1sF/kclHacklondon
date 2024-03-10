1fc86d5bf170b303a094629f5a1d769935f3ecc2
Av1sF <88183334+Av1sF@users.noreply.github.com>
2024-03-10 15:18:53 +0000

    nearly there

--- app3.py
+++ app3.py
@@ -2,29 +2,41 @@
 from flask import Flask, render_template, url_for, request
 from flask_socketio import SocketIO
 import requests
+import os
+
+
 
 unitTopic = {
     
-    "CCP":"""Find resources on the internet that are primarily related to structure and function of a processor, types of processor, input, output and storage, give links for A-level related resources for students""", 
+    "CCP":"structure and function of a processor, types of processor, input, output and storage", 
 
-    "SSD":"""Find resources on the internet that are primarily related to system software, application deneration, software development and types of programming languages, give links for A-level related resources for students""",
+    "SSD":"system software, application deneration, software development and types of programming languages",
     
-    "ED" :"""Find resources on the internet that are primarily related to compression, encryption and hashing, databases, networks and web technologies, give links for A-level related resources for students""",
+    "ED" :"compression, encryption and hashing, databases, networks and web technologies",
    
-    "DDA": """Find resources on the internet that are primarily related to boolean algebra, data types and data structures, give links for A-level related resources for students""",
-    "LMCE":"""Find resources on the internet that are primarily related to computing-related legislation and moral and ethical issues, give links for A-level related resources for students""",
+    "DDA": "boolean algebra, data types and data structures",
+    "LMCE":"computing-related legislation and moral and ethical issues",
 
-    "ECT": """Find resources on the internet that are primarily related to elements of computational thinking, give links for A-level related resources for students"""
+    "ECT": "elements of computational thinking"
 }
 
-API_URL = "https://www.stack-inference.com/inference/v0/run/3d4904b4-95d8-4f91-8413-598cd6ad6e28/65ec7764f55f2d6ba176042d"
-headers = {'Authorization':
+
+
+"""
+API keys 
+"""
+
+# StackAI/perplexity API 
+# another interface is used because perplexity AI tokens limit is lower 
+PERPLEX_API_URL = "https://www.stack-inference.com/inference/v0/run/3d4904b4-95d8-4f91-8413-598cd6ad6e28/65ec7764f55f2d6ba176042d"
+perplex_headers = {'Authorization':
 			 'Bearer d6c868e4-dd77-433a-b8f6-40264a0fd870',
 			 'Content-Type': 'application/json'
 		}
 
-API_URL1 = "https://www.stack-inference.com/inference/v0/run/87a0f031-97dd-4577-a4c9-93c4ae154122/65ed800431b5c269a6c0b018"
-headers1 = {'Authorization':
+#StackAI API
+API_URL = "https://www.stack-inference.com/inference/v0/run/87a0f031-97dd-4577-a4c9-93c4ae154122/65ed800431b5c269a6c0b018"
+headers = {'Authorization':
 			 'Bearer 5064899d-f14e-4837-877f-e2b700d63f70',
 			 'Content-Type': 'application/json'
 		}
@@ -32,44 +44,52 @@ headers1 = {'Authorization':
 """
 Processing functions 
 """
+
+# process user's mock answers 
 def processMock(form):
-    ans1 = request.form['Name']
-    ans2 = request.form['Email']
-    ans3 = request.form['Message']
-    string = f"{ans1}, {ans2}, {ans3}"
-    file = open('C:\\Users\\ROG\\kclHacklondon\\txtOutputs\\11.1.1. Structure and Function of the Processor','r')
-    str=''
+
+    ans1 = request.form['q1']
+    ans2 = request.form['q2']
+    ans3 = request.form['q3']
+
+    answers = f"{ans1}, {ans2}, {ans3}"
+
+    # concatenate lines in file into  string
+    file = open(os.path.abspath("txtOutputs\\11.1.1. Structure and Function of the Processor"),'r')
+    context=''
     for x in file:
-        str+=x
-    output = query1({"string-1":string,'int-0':'Generate 5 A level type question with options','in-0':"Give a detailed summary of all the topics"
-                    ,'string-0':str})
-    
-    print(output['outputs']['out-2'] )
+        context+=x
+
+    # query StackAI to compare student answers to markscheme, identify weak areas; providing a summary on weak topics.  
+    output = query({"string-1":answers,'int-0':'Generate 5 A level type question with options','in-0':"Give a detailed summary of all the topics"
+                    ,'string-0':context})
+ 
     return (output['outputs']['out-2'],output['outputs']['out-0'] )
 
+# query with perplexity AI 
+def plex_query(payload):
+ response = requests.post(API_URL, headers=headers, json=payload)
+ return response.json()
 
-
+# query with stackAI
 def query(payload):
  response = requests.post(API_URL, headers=headers, json=payload)
  return response.json()
 
-def query1(payload):
- response = requests.post(API_URL1, headers=headers1, json=payload)
- return response.json()
-
-def getQuestion():
-    output = query1({"in-2": """Generate 5 A-level objective type questions from the following information
-(give A ,B,C,D). List all the questions below Add a slash after listing all options to seperate each question from the previous one.""", 
-"user_id": """<USER or Conversation ID>"""})
-    
-    lst = output['outputs']['out-3'].split(" /")
-    print(lst)
-    return tuple(lst)
-
+# Suggests and provides resources to user about specific topics of an 
 def getResource(prompt):
-    output = query1({"in-3":prompt,
+
+    # concatenate lines in file into single string
+    file = open(os.path.abspath("txtOutputs\\163.4. Searching Algorithms - Concise"), encoding='utf-8')
+    context=''
+    for x in file:
+        context+=x
+
+    # query genAI to generate a list of suitable resources 
+    output = query({"in-1":"Find A-level resources (including links) for the following topics",'string-0':context,
                 "user_id":"""<USER or Conversation ID>"""})
-    return (output['outputs']['out-4'])
+    
+    return (output['outputs']['out-1'])
 
 
 """
@@ -83,44 +103,37 @@ sio = SocketIO(app)
 
 # templates to serve for each webpage 
 
+# main page
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    initaliseMock = False
+    # function handles form data from mocks only
     mockResults = False
     topic = False
-    question = False
 
     if request.method == 'POST':
         form = request.form
         mockResults = processMock(form)
 
-    return render_template('test.html', mockResults=mockResults, topic=topic, question=question)
+    return render_template('test.html', mockResults=mockResults, topic=topic)
 
 @app.route('/topic', methods=['GET', 'POST'])
 def topic():
+    # function only handles form data from the topic subsection 
     mockResults = False 
     topic = False
-    question = False
 
     if request.method == 'POST':
         form = request.form
         topics = unitTopic.get(request.form['submit_button'])
         topic = getResource(topics)
+        topic = [i for i in topic]
+        for i in range(0, len(topic)-1, 112):
+            topic.insert(i, "\n")
+        topic = ''.join(e for e in topic)
         print(topic)
 
-    return render_template('test.html', mockResults=mockResults, topic=topic, question=question)
+    return render_template('test.html', mockResults=mockResults, topic=topic)
 
-@app.route('/question', methods=['GET', 'POST'])
-def question():
-    mockResults = False 
-    topic = False
-    question = False
-
-    if request.method == 'POST':
-        form = request.form
-        question = getQuestion()
-
-    return render_template('test.html', mockResults=mockResults, topic=topic, question=question)
 
 """
 SocketIO event handlers 
